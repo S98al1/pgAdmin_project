6aaeeaa50ac8c6ca57c706034a0a2631745dd2b3
S98al1 <alistairsimpson5@gmail.com>
2025-07-17 20:00:28 +0100

    Add files via upload

--- pgAdmin project.sql
+++ pgAdmin project.sql
@@ -1,5 +1,7 @@
 SET search_path TO portfolio;
 
+-- table creation
+
 Create table customers(
 customer_id INT,
 first_name TEXT,
@@ -42,7 +44,7 @@ stock_quantity INTEGER
 );
 
 
-select * from customers;
+-- data cleaning
 
 Alter table customers
 Add primary key (customer_id);
@@ -59,24 +61,99 @@ alter table products
 alter column price type float(2)
 ;
 
+
+-- rank of customers by amount spent
+
+WITH customer_spending AS (
+    SELECT 
+        c.customer_id,
+        CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+        ROUND(SUM(oi.quantity * oi.unit_price)::numeric, 2) AS total_spent
+    FROM customers c
+    JOIN orders o ON c.customer_id = o.customer_id
+	JOIN order_items oi ON o.order_id = oi.order_id
+    GROUP BY c.customer_id, customer_name
+)
 SELECT 
-    c.customer_id,
-    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
-	ROUND(SUM(oi.quantity * oi.unit_price)::numeric, 2) AS total_spent
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_items oi ON o.order_id = oi.order_id
-JOIN products p ON oi.product_id = p.product_id
-GROUP BY c.customer_id, customer_name
-ORDER BY total_spent DESC
-;
+    customer_name,
+    total_spent,
+    RANK() OVER (ORDER BY total_spent DESC) AS spend_rank
+FROM customer_spending
+ORDER BY spend_rank;
+
+
+
+-- total and average of all sales
 
 SELECT
-    DATE_TRUNC('month', o.order_date) AS month,
-    ROUND(SUM(oi.quantity * oi.unit_price)::numeric, 2) AS total_spent
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    ROUND(SUM(oi.quantity * oi.unit_price)::numeric, 2) AS total_sales,
+    ROUND(SUM(oi.quantity * oi.unit_price)::numeric / COUNT(DISTINCT o.order_id), 2) AS avg_order_value
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id;
+
+
+-- avg order value by customer ranked descending
+
+SELECT
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    ROUND(SUM(oi.quantity * oi.unit_price)::numeric, 2) AS total_sales,
+    ROUND(SUM(oi.quantity * oi.unit_price)::numeric / COUNT(DISTINCT o.order_id), 2) AS avg_order_value
 FROM customers c
 JOIN orders o ON c.customer_id = o.customer_id
 JOIN order_items oi ON o.order_id = oi.order_id
-JOIN products p ON oi.product_id = p.product_id
-GROUP BY DATE_TRUNC('month', o.order_date)
-ORDER BY month;
+GROUP BY c.customer_id, customer_name
+ORDER BY avg_order_value DESC;
+
+-- MoM Sales
+
+WITH monthly_totals AS (
+    SELECT
+        TO_CHAR(DATE_TRUNC('month', o.order_date), 'YYYY-MM') AS month,
+        COUNT(DISTINCT o.order_id) AS total_orders,
+        SUM(oi.quantity * oi.unit_price)::numeric AS total_sales,
+        ROUND(SUM(oi.quantity * oi.unit_price)::numeric / COUNT(DISTINCT o.order_id), 2) AS avg_order_value
+    FROM orders o
+    JOIN order_items oi ON o.order_id = oi.order_id
+    GROUP BY DATE_TRUNC('month', o.order_date)
+),
+ranked_months AS (
+    SELECT *,
+           RANK() OVER (ORDER BY month DESC) AS month_rank
+    FROM monthly_totals
+)
+SELECT 
+    month,
+    total_orders,
+    total_sales,
+    avg_order_value
+FROM ranked_months
+ORDER BY month DESC;
+
+-- YoY Sales Totals
+
+WITH yearly_totals AS (
+    SELECT
+        TO_CHAR(DATE_TRUNC('year', o.order_date), 'YYYY') AS year,
+        COUNT(DISTINCT o.order_id) AS total_orders,
+        SUM(oi.quantity * oi.unit_price)::numeric AS total_sales,
+        ROUND(SUM(oi.quantity * oi.unit_price)::numeric / COUNT(DISTINCT o.order_id), 2) AS avg_order_value
+    FROM orders o
+    JOIN order_items oi ON o.order_id = oi.order_id
+    GROUP BY DATE_TRUNC('year', o.order_date)
+),
+ranked_years AS (
+    SELECT *,
+           RANK() OVER (ORDER BY year DESC) AS year_rank
+    FROM yearly_totals
+)
+SELECT 
+    year,
+    total_orders,
+    total_sales,
+    avg_order_value
+FROM ranked_years
+ORDER BY year DESC;
